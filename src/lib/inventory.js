1880302d333d18156704998085494e3a1b1c08bb
slickgame <slickpokefangame@gmail.com>
2026-03-14 08:54:59 -0400

    Add inventory module with default values and function

---
+++ src/lib/inventory.js
@@ -0,0 +1,12 @@
+export const EMPTY_INVENTORY = {
+  potion: 0,
+  revive: 0,
+  bait: 0,
+};
+
+export function withInventoryDefaults(inventory) {
+  return {
+    ...EMPTY_INVENTORY,
+    ...(inventory ?? {}),
+  };
+}
